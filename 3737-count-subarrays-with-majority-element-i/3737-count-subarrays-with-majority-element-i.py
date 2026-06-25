97421b5a27b7025fc24e52a54881fd13de3fe11b
Aditya Kushwaha <146941579+aditya00801@users.noreply.github.com>
2026-06-25 12:54:13 +0530

    Time: 1910 ms (32%), Space: 19.5 MB (30.22%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
@@ -1,18 +1,31 @@
 class Solution:
     def countMajoritySubarrays(self, nums, target):
+        # Get the size of the array
         n = len(nums)
+
+        # Stores the final answer
         ans = 0
 
+        # Choose the starting index of every subarray
         for i in range(n):
+
+            # Number of times target appears in the current subarray
             count = 0
 
+            # Extend the subarray from i to j
             for j in range(i, n):
+
+                # Update target count if current element is target
                 if nums[j] == target:
                     count += 1
 
+                # Current subarray length
                 length = j - i + 1
 
+                # Majority condition:
+                # target_count > half of subarray length
                 if count > length // 2:
                     ans += 1
 
+        # Return total valid subarrays
         return ans
