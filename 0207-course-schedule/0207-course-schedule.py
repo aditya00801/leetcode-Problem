ba53803478e5fcff0b19ba6d4566aea3c4022f89
Aditya Kushwaha <146941579+aditya00801@users.noreply.github.com>
2026-08-14 19:46:05 +0530

    Time: 0 ms (100%), Space: 20.6 MB (53.25%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        # Build graph
+        for course, prerequisite in prerequisites:
+            graph[prerequisite].append(course)
+            indegree[course] += 1
+
+        # Courses with no prerequisites
+        queue = deque()
+
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+
+        completed = 0
+
+        # Topological sort
+        while queue:
+            course = queue.popleft()
+            completed += 1
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        return completed == numCourses
